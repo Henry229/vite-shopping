cbc9419c705f83808bbc9eb3ee013a94a37246b3
Henry <chunhyungjoon@gmail.com>
2024-07-14 20:23:16 +1000

    chore: Update MongoDB connection to use environment variable

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,14 +3,14 @@ const app = express();
 const port = 4000;
 const cors = require('cors');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(
-    'mongodb+srv://Henry:eaz1RpBaa8ks0orF@cluster0.e5uomlj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
-  )
+  .connect(process.env.MONGODB_URI)
   .then(() => {
     console.log('Connected to MongoDB');
   })
